9fe5d26aa9786b21296ca95f61472e40a2f9a035
kazsaj <ksajkowski@gmail.com>
2018-08-28 22:30:44 +0100

    spawning multiple threads into a vector

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,23 @@ mod worker;
 use std::thread;
 use std::time::Duration;
 
-
 fn main() {
     println!("Starting main thread");
-    let handle = thread::spawn(|| worker::main());
-    for i in 1..5 {
-        println!("main {}", i);
-        thread::sleep(Duration::from_millis(1));
+
+    let configured_threads_count = 2;
+    let mut threads: Vec<thread::JoinHandle<_>> = Vec::new();
+
+    for i in 0..configured_threads_count {
+        println!("Spawning worker thread {}", i);
+        threads.push(thread::spawn(|| worker::main()));
     }
-    handle.join().unwrap();
-    println!("Finishing main thread");
+
+    // more main thread logic here
+
+    for j in 0..threads.len() {
+        println!("joining worker {}", j);
+        let mut popped_thread = threads.pop();
+        popped_thread.unwrap().join();
+    }
+
 }
