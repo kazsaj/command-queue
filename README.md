b1a9c86f36f0359df9e61b362ea91a941f9f470f
kazsaj <ksajkowski@gmail.com>
2019-05-13 19:13:24 +0100

    Popping integers from a list

---
+++ README.md
@@ -0,0 +1,11 @@
+## TODO
+- (OK) connect to redis
+- (OK) connect to redis in each child thread
+- read connection details from some configuration file
+- (OK) get one element from work list
+- decode JSON element from a work list
+
+## Useful redis commands
+- MONITOR
+- RPUSH key value - add element to the list
+- BLPOP key - remove first element from a list, or block until you find one
--- src/worker.rs
+++ src/worker.rs
@@ -1,17 +1,25 @@
-use config;
+extern crate redis;
+
 use std::thread;
 use std::time::Duration;
+use worker::redis::Commands;
 
 pub fn main()
 {
-    // actual worker logic here
-
-    let config_value = config::get();
-    println!("Config, which is empty: {}", config_value);
-
+    let queue_name = "queue";
     for i in 1..10 {
-        println!("worker {}", i);
+        println!("worker pull number {}", i);
         thread::sleep(Duration::from_millis(1));
+        let pulled_value = pop_from_queue(queue_name);
+        match pulled_value {
+            Ok(value) => println!("got a value: {}", value.1),
+            Err(value) => println!("an error occurred: {}", value),
+        }
     }
+}
 
+fn pop_from_queue(queue_name: &str) -> redis::RedisResult<(String, isize)> {
+    let client = redis::Client::open("redis://command_queue_redis/")?;
+    let connection = client.get_connection()?;
+    connection.blpop(queue_name, 10)
 }
